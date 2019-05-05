54b14ccc5fff79e79a28ecb7e9e4584f992ea299
PR3R00T <PR3R00T@protonmail.com>
2019-05-05 14:48:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SNDBOX-LIGHT-API
-A Light wrapper for the SNDBOX Rest API for Python3.
+A Light wrapper for the SNDBOX Rest API written in Python3.
+This wrapper can be used to communicate to the SNDBOX exposed Rest API to all for the submissions of samples to the platform.
+As per the documentation found at: https://app.sndbox.com/docs/api The wrapper allows for the three functions:
+    Search - Search the database for a md5hash.
+    Submit - Submit a sample to the platform.
+    Metadata - Gather information about a uploaded file.
+
+In order to use the API you must first obtain a API Key, This is found by registering a account with SNDBOX(https://app.sndbox.com/register)
+Then navigating to the profile section.
+
+Below are the requirements for each function:
+    search(apikey='123',md5hash='1bed6d8bdf2c1b7fd5e6badc08e7634a')
+    submit(apikey='123',file='testsample.docx',email=False)    
+    metadata(apikey='123',id='12345-33422-23134-122')
+
+Providing all input is correct each function returns the full raw response from the SNDBOX Rest API.
+    
---
+++ SNDBOX.py
@@ -0,0 +1,99 @@
+import requests
+
+
+def search(apikey,md5hash):
+    '''
+    Search the SNDBOX Database for Hash of Sample.
+    Input:
+    API Key - For authenication
+    md5hash - For sample check
+    Output:
+    Response from check in json format if parameters are vaild.
+    '''
+    #Prep Call
+    try:
+        response = requests.get("https://api.sndbox.com/developers/files/verdict?apikey="+apikey+"&hash="+md5hash)
+    except exception as e:
+        print("Error Occurred connecting to the SNDBOX API - " + str(e))
+    #Check Response
+    if response.status_code == 200 or response.status_code == 404:
+        return response
+    elif response.status_code == 400:
+        error = "Bad Parameter, Error with MD5 Hash Format"
+        return error
+    elif response.status_code == 401:
+        error = "API Key incorrect"
+        return error
+
+
+
+def file_check(file):
+    '''
+    Check a sample if it is supported on the SNDBOX enviroment
+    Input:
+    File - string name of File
+    output:
+    Boolen - True/False
+    '''
+    #File Extension Check
+    supported_files=[".doc",".dot",".xls",".csv",".xlt",".xlm",".ppt",".pot",".pps",".docx",".docm",".dotx",".dotm",".dotm",".xlsx",".xlsm",".xltx",".xltm",".xlsb",".xla",".xlam",".iqy",".pptx",".pptm",".potx",".ppsx",".xml",".pe32",".rtf",".pdf",".vbs",".vbe",".ps1",".js",".lnk",".html",".bat",".zip"]
+    supported_sample = False
+    while supported_sample == False:
+        if "." not in file:
+            supported_sample = True
+            return True
+        else:
+            for file_ext in supported_files:
+                if file_ext in file:
+                    supported_sample = True
+                    return True
+                else:
+                    return False
+
+
+
+def submit(apikey,file,email):
+    '''
+    submit a File to the SNDBOX Database.
+    input:
+    API Key - authentication
+    File - Sample to test
+    email - Optional - email to send alert to after processed Sample True/False
+    output:
+    response - JSON format of uploaded file
+    '''
+    #Check File is supported
+    file_supported = file_check(file)
+    if file_supported == False:
+        error = "File is not supported, Please check Documentation at: https://app.sndbox.com/docs/files"
+        return error
+    else:
+    #Supported File must be true, Continue with the upload
+        sndbox_api_url ='https://api.sndbox.com/developers/files?apikey=' + apikey
+        files =  {'file':open(file,'rb')}
+        email_flag = {'emailnotification':email}
+        try:
+            response = requests.post(url=sndbox_api_url, files=files,data=email_flag)
+        except exception as e:
+            print("Error Occurred connected to the SNDBOX API - " + str(e))
+        return response
+
+
+
+def metadata(apikey,id):
+    '''
+    Get metadata of a submitted file from SNDBOX Database.
+    input:
+    apikey - authentication
+    id - unique ID of the sample, found in response of submitting a sample.
+    output:
+    response - Metadata as JSON
+    '''
+    url = 'https://api.sndbox.com/developers/files/'+ id + '?apikey=' + apikey
+    try:
+        response = requests.get(url)
+    except exception as e:
+        print("Error Occurred connecting to SNDBOX API - " + str(e))
+    return response 
+
+
